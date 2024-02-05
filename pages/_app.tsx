eaf21c6d7e5f7e1b24057a518b1fcb28df472278
Goose-Win11 <64352864+Goose-Of-War@users.noreply.github.com>
2024-02-05 20:38:26 +0530

    Analytics?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout';
@@ -7,5 +8,6 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }: AppProps) {
 	return (<Layout>
 		<Component {...pageProps} />
+		<Analytics />
 	</Layout>);
 }
